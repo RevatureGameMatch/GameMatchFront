5f8208ee2dba0d8cb18d4e2b0537c4b54236c734
Kayla Filio <kfilio97@gmail.com>
2020-04-04 22:51:48 -0400

    Added basic HTML to createAccount

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -7,6 +8,7 @@ import { AboutG2GComponent } from './components/about-g2-g/about-g2-g.component'
 import { CreateAccountComponent } from './components/create-account/create-account.component';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
+
 
 @NgModule({
   declarations: [
@@ -18,7 +20,8 @@ import { HomeComponent } from './components/home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/models/users.ts
+++ src/app/models/users.ts
@@ -5,13 +5,15 @@ export class User {
     firstName: string;
     lastName: string; 
     email: string;
+    type: number;
 
-    constructor(username: string, password: string, firstName: string, lastName: string, email: string){
+    constructor(username: string, password: string, firstName: string, lastName: string, email: string, type: number){
         this.id = 0;
         this.username = username;
         this.password = password;
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
+        this.type = type;
     }
 }
