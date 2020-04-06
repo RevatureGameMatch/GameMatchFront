1b06a8cd137b21f1d71c7086a77f1e7854e3f19a
nyuan13 <yuan.nancy1@gmail.com>
2020-04-05 23:24:42 -0400

    added routing for create room, join room, and surveys

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,14 +6,20 @@ import { CreateAccountComponent } from './components/create-account/create-accou
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { JoinRoomComponent } from './components/join-room/join-room.component';
+import { CreateRoomComponent } from './components/create-room/create-room.component';
+import { SurveysComponent } from './components/surveys/surveys.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent},
-  { path: 'about', component: AboutG2GComponent},
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutG2GComponent },
   { path: 'signup', component: CreateAccountComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'dashboard', component: DashboardComponent},
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'join-room', component: JoinRoomComponent },
+  { path: 'create-room', component: CreateRoomComponent },
+  { path: 'surveys', component: SurveysComponent },
 ]
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,9 @@ import { CreateAccountComponent } from './components/create-account/create-accou
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { CreateRoomComponent } from './components/create-room/create-room.component';
+import { JoinRoomComponent } from './components/join-room/join-room.component';
+import { SurveysComponent } from './components/surveys/surveys.component';
 
 @NgModule({
   declarations: [
@@ -19,6 +22,9 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
     LoginComponent,
     HomeComponent,
     DashboardComponent,
+    CreateRoomComponent,
+    JoinRoomComponent,
+    SurveysComponent,
   ],
   imports: [
     BrowserModule,
