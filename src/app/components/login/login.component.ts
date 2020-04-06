201225bd959d2265bfa894fba4ed53ad20376e45
nyuan13 <yuan.nancy1@gmail.com>
2020-04-05 20:56:26 -0400

    login component complete

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { StorageMap } from '@ngx-pwa/local-storage';
 import { LoginService } from 'src/app/services/login.service';
-import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 import { User } from 'src/app/models/users';
 
 @Component({
@@ -11,15 +11,19 @@ import { User } from 'src/app/models/users';
 })
 export class LoginComponent implements OnInit {
 
-  loginForm;
+  loginForm: FormGroup;
+
+  // Validates that username and password entries are not null
+  username = new FormControl('', Validators.required);
+  password = new FormControl('', Validators.required);
 
   constructor(
     private storage: StorageMap,
     private loginService: LoginService,
     private formBuilder: FormBuilder,) {
       this.loginForm = this.formBuilder.group({
-        username: '',
-        password: '',
+        username: this.username,
+        password: this.password,
       })
      }
 
