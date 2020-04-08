65c41b931bee69c6a8fef092015e13bcfa953c3a
Kayla Filio <kfilio97@gmail.com>
2020-04-07 22:15:22 -0400

    Created basic html for Sign up page

--- src/app/components/create-account/create-account.component.ts
+++ src/app/components/create-account/create-account.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountForm } from 'src/app/models/account-form';
 
 @Component({
   selector: 'app-create-account',
   templateUrl: './create-account.component.html',
   styleUrls: ['./create-account.component.css']
 })
-export class CreateAccountComponent implements OnInit {
+export class CreateAccountComponent /*implements OnInit*/ {
 
-  constructor() { }
+  type = ['User', 'Moderator', 'Admin'];
 
-  ngOnInit(): void {
-  }
+  submitted = false;
+
+  onSubmit() {this.submitted = true;}
+
+  model = new AccountForm('','','','','','');
+
+  newAccountForm(){
+    this.model = new AccountForm('','','','','','');
+  } 
+
+  // constructor() { }
+
+  // ngOnInit(): void {
+  // }
 
 }
---
+++ src/app/models/account-form.ts
@@ -0,0 +1,12 @@
+export class AccountForm {
+
+    constructor(
+        public firstName: string,
+        public lastName: string,
+        public email: string,
+        public username: string,
+        public password: string,
+        public type: string
+    ){ }
+
+}
