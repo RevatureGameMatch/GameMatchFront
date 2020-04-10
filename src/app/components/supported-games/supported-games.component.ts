1d631a8a63f429d9bb6bd3c0d72b592b01fabe49
nyuan13 <yuan.nancy1@gmail.com>
2020-04-09 22:52:07 -0400

    completed supported games page

---
+++ src/app/components/supported-games/supported-games.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { StorageMap } from '@ngx-pwa/local-storage';
+import { GetGamesService } from 'src/app/services/get-games.service';
+import { Observable } from 'rxjs';
+import { User } from 'src/app/models/users';
+import { Game } from 'src/app/models/game';
+
+@Component({
+  selector: 'app-supported-games',
+  templateUrl: './supported-games.component.html',
+  styleUrls: ['./supported-games.component.css']
+})
+export class SupportedGamesComponent implements OnInit {
+  user$: Observable<User>;
+  user: User;
+  games: Game[];
+
+  constructor(
+    private storage: StorageMap,
+    private gameService: GetGamesService,
+  ) { }
+
+  ngOnInit(): void {
+    // @ts-ignore
+    this.user$ = this.storage.get<User>('currentUser');
+    this.user$.subscribe(
+      (result) => {
+        this.user = result
+        console.log(this.user);
+      }
+    );
+
+    this.gameService.getGames().subscribe(
+      (result) => {
+        this.games = result;
+        console.log(this.games);
+      }
+    )
+  }
+
+
+}
